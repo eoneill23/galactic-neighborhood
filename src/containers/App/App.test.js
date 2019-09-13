b93c2624f54fbcc02a2a9dffe80d5a1c926ab6f3
Eric O'Neill <oneill.eric23@gmail.com>
2019-09-13 11:58:42 -0600

    Attempt (and fail) at router test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router';
 import { App, mapStateToProps } from './App';
+import HomePage from '../HomePage/HomePage';
 
 describe('App', () => {
   let wrapper, mockPlanetFacts;
@@ -31,6 +33,58 @@ describe('App', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('mapStateToProps', () => {
+    const mockState = {
+      apod: { url: 'https://apod.nasa.gov/apod/image/1909/HarvestmoonGraffand.jpg' },
+      apodError: '',
+      iss: { latitutde: 1, longitude: 2, velocity: 27000 },
+      issError: '',
+      asteroids: [
+        {
+          name: '(U2)',
+          min_diameter: 2,
+          max_diameter: 3
+        },
+        {
+          name: '(K2)',
+          min_diameter: 1,
+          max_diameter: 4
+        }
+      ],
+      asteroidsError: '',
+      planetFacts: {
+        'Mercury': {
+          fact: 'The closest planet to the Sun does indeed have ice on its surface. That sounds surprising at first glance, but the ice is found in permanently shadowed craters — those that never receive any sunlight. It is thought that perhaps comets delivered this ice to Mercury in the first place. In fact, NASA’s MESSENGER spacecraft not only found ice at the north pole, but it also found organics, which are the building blocks for life. Mercury is way too hot and airless for life as we know it, but it shows how these elements are distributed across the Solar System.',
+          factUrl: 'https://www.universetoday.com/33415/interesting-facts-about-the-planets/',
+          imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg'
+        }
+      }
+    }
+
+    const expectedPlanetFacts = {
+      'Mercury': {
+        fact: 'The closest planet to the Sun does indeed have ice on its surface. That sounds surprising at first glance, but the ice is found in permanently shadowed craters — those that never receive any sunlight. It is thought that perhaps comets delivered this ice to Mercury in the first place. In fact, NASA’s MESSENGER spacecraft not only found ice at the north pole, but it also found organics, which are the building blocks for life. Mercury is way too hot and airless for life as we know it, but it shows how these elements are distributed across the Solar System.',
+        factUrl: 'https://www.universetoday.com/33415/interesting-facts-about-the-planets/',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg'
+      }
+    }
+
+    expect(mapStateToProps(mockState).planetFacts).toEqual(expectedPlanetFacts);
+  });
+
 });
 
+// describe('Route', () => {
+//   it('should route to the home page by default', () => {
+
+//     const wrapper = mount(
+//       <MemoryRouter initialEntries={['/']}>
+//         <App />
+//       </MemoryRouter>
+//     );
+
+//     expect(wrapper.find(HomePage)).toHaveLength(1);
+//   });
+// });
 
