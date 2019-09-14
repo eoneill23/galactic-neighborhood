c877ab3a719cc76019ffe1d965e1f07c7ae2e1a3
Eric O'Neill <oneill.eric23@gmail.com>
2019-09-14 14:01:18 -0600

    Add active and hover styling to links and take header snapshot

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -7,16 +7,16 @@ const Header = () => {
     <>
     <h1>Galactic Neighborhood</h1>
     <header className='header'>
-      <NavLink className='nav-link' to='/'>Home</NavLink>
-      <NavLink className='nav-link' to='/mercury'>Mercury</NavLink>
-      <NavLink className='nav-link' to='/venus'>Venus</NavLink>
-      <NavLink className='nav-link' to='/earth'>Earth</NavLink>
-      <NavLink className='nav-link' to='/mars'>Mars</NavLink>
-      <NavLink className='nav-link' to='/jupiter'>Jupiter</NavLink>
-      <NavLink className='nav-link' to='/saturn'>Saturn</NavLink>
-      <NavLink className='nav-link' to='/uranus'>Uranus</NavLink>
-      <NavLink className='nav-link' to='/neptune'>Neptune</NavLink>
-      <NavLink className='nav-link' to='/pluto'>Pluto</NavLink>
+      <NavLink activeClassName='active' className='nav-link' exact to='/'>Home</NavLink>
+        <NavLink activeClassName='active' className='nav-link' to='/mercury'>Mercury</NavLink>
+        <NavLink activeClassName='active' className='nav-link' to='/venus'>Venus</NavLink>
+        <NavLink activeClassName='active' className='nav-link' to='/earth'>Earth</NavLink>
+        <NavLink activeClassName='active' className='nav-link' to='/mars'>Mars</NavLink>
+        <NavLink activeClassName='active' className='nav-link' to='/jupiter'>Jupiter</NavLink>
+        <NavLink activeClassName='active' className='nav-link' to='/saturn'>Saturn</NavLink>
+        <NavLink activeClassName='active' className='nav-link' to='/uranus'>Uranus</NavLink>
+        <NavLink activeClassName='active' className='nav-link' to='/neptune'>Neptune</NavLink>
+        <NavLink activeClassName='active' className='nav-link' to='/pluto'>Pluto</NavLink>
     </header>
     </>
   )
