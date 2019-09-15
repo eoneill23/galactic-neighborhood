659ea466158ab19291b819c2587e9a46523ef9e0
Eric O'Neill <oneill.eric23@gmail.com>
2019-09-15 12:08:26 -0600

    Update WeightForm snapshot and write tests for addAPOD, addAPODError, addISS

---
+++ src/actions/index.test.js
@@ -0,0 +1,48 @@
+import { addAPOD, addAPODError, addISS, addISSError, addAsteroids, addAsteroidsError, addCalculation, resetStoreCalculation } from './index';
+
+describe('Actions', () => {
+
+  it('should have a type of ADD_APOD', () => {
+
+    let APOD = {
+      name: 'iss',
+      lat: 1,
+      long: 2
+    }
+
+    let expectedAction = {
+      type: 'ADD_APOD',
+      APOD
+    }
+
+    expect(addAPOD(APOD)).toEqual(expectedAction);
+  });
+
+  it('should have a type of APOD_ERROR', () => {
+
+    let error = 'Issue fetching APOD.'
+
+    let expectedAction = {
+      type: 'APOD_ERROR',
+      error
+    }
+
+    expect(addAPODError(error)).toEqual(expectedAction)
+  });
+
+  it('should have a type of ADD_ISS', () => {
+
+    let ISS = {
+      name: 'ISS',
+      lat: 1,
+      long: 2
+    }
+
+    let expectedAction = {
+      type: 'ADD_ISS',
+      ISS
+    }
+
+    expect(addISS(ISS)).toEqual(expectedAction)
+  });
+});
