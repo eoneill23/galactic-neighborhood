659433867082ecebbaa3bd5325247ae7e7df7639
Eric O'Neill <oneill.eric23@gmail.com>
2019-09-14 18:21:58 -0600

    Finalize testing, including mapStateToProps

--- src/containers/WeightForm/WeightForm.test.js
+++ src/containers/WeightForm/WeightForm.test.js
@@ -21,5 +21,55 @@ describe('WeightForm', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  
+  it('should update state on a change in the input', () => {
+
+    const mockEvent = {target: {name: 'number', value: 100}}
+
+    wrapper.find('input').simulate('change', mockEvent);
+
+    expect(wrapper.state('number')).toEqual(100);
+  });
+
+  it('should call handleSubmit on a button click', () => {
+
+    const mockEvent = {preventDefault: jest.fn()}
+
+    wrapper.instance().handleSubmit = jest.fn();
+    wrapper.instance().clearInputs = jest.fn();
+    wrapper.instance().forceUpdate();
+    wrapper.find('button').simulate('click', mockEvent);
+
+    expect(wrapper.instance().handleSubmit).toHaveBeenCalled();
+  });
+
+  it('should clear the inputs when clearInputs is called', () => {
+
+    wrapper.setState({number: 100});
+
+    wrapper.instance().clearInputs();
+
+    expect(wrapper.state('number')).toEqual(0)
+  });
+
+  describe('mapStateToProps', () => {
+
+    it('should return an object with the current calculation', () => {
+
+      let mockState = {
+        apod: {name: 'APOD', url: 'google.com'},
+        apodError: '',
+        iss: {name: 'iss', lat: 2, long: 3},
+        issError: '',
+        asteroids: [{id: 1}, {id: 2}],
+        asteroidsError: '',
+        planetFacts: { 'Mercury': {fact: 'closest to the sun'}},
+        calculation: 100
+      }
+
+      const expected = {calculation: 100};
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected)
+    });
+  });
 });
